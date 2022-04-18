8a8073e9044367b3531e83ae029ea9ab08cb9f84
dima-sheiko <sheydim@gmail.com>
2022-04-18 15:22:31 +0300

    Finished project

---
+++ script.js
@@ -0,0 +1,17 @@
+let countEl = document.querySelector("#count-el");
+let saveEl = document.querySelector("#save-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+
